3995660c566c65da3b48a5deae6ec96971938398
Christian <christian.gernsoe@gmail.com>
2020-06-13 14:49:37 +0200

    Added new color

--- sproutsDisplay.py
+++ sproutsDisplay.py
@@ -9,7 +9,7 @@ class SproutsDisplay:
     RED = (255, 0, 0)
     LIGHT_RED = (200, 0, 0)
     PURPLE = (255, 0, 255)
-    BLUE = (0, 128, 128)
+    BLUE = (51, 212, 255)
     LIGHT_BLUE = (0, 0, 200)
 
     def __init__(self, screenwidth, screenheight,pygame):
